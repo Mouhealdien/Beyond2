d377bf18daf3642f70db8ba03e90caa196fe141b
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-05-31 23:15:33 +0300

    adding captcha and some edits

--- components/global/contact/ContactForm.tsx
+++ components/global/contact/ContactForm.tsx
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Input from "../Input";
 import { useTranslation } from "next-i18next";
 import { useSendEmailMutation } from "../../../lib/redux/services/api";
 import { ContactFormData } from "../../../common/types/contact";
 import { Controller, useForm } from "react-hook-form";
-import Select from "react-select";
-
+import Select, { OptionsOrGroups, GroupBase } from "react-select";
+import ReCAPTCHA from "react-google-recaptcha";
+import { toast } from "react-toastify";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 interface OptionType {
   value: string;
   label: string;
 }
 const ContactForm = () => {
   const { i18n, t } = useTranslation();
-
+  const recaptcha = useRef();
   const selectStyle = {
     placeholder: (provided) => ({
       ...provided,
@@ -42,7 +45,10 @@ const ContactForm = () => {
     },
   });
 
-  const questionRegardingOptions = [
+  const questionRegardingOptions: OptionsOrGroups<
+    OptionType,
+    GroupBase<OptionType>
+  > = [
     { value: "socialMediaManagement", label: t("socialMediaManagement") },
     {
       value: "marketingStrategyDevelopment",
@@ -52,14 +58,14 @@ const ContactForm = () => {
     { value: "designAndProduction", label: t("designAndProduction") },
   ];
 
-  const hearUsOptions = [
+  const hearUsOptions: OptionsOrGroups<OptionType, GroupBase<OptionType>> = [
     { value: "searchEngines", label: t("searchEngines") },
     { value: "socialMedia", label: t("socialMedia") },
     { value: "referrals", label: t("referrals") },
     { value: "advertising", label: t("advertising") },
   ];
 
-  const timeOptions = [
+  const timeOptions: OptionsOrGroups<OptionType, GroupBase<OptionType>> = [
     { value: "morning", label: t("morning") },
     { value: "afternoon", label: t("afternoon") },
     { value: "evening", label: t("evening") },
@@ -81,7 +87,14 @@ const ContactForm = () => {
   });
 
   const onSubmit = async (data) => {
-    console.log(data);
+    const token = recaptcha.current.getValue();
+    if (token) {
+      console.log("Captcha checked");
+      toast.success("we will reply you soon");
+      console.log(data);
+    } else {
+      toast.error("please check the reCaptcha");
+    }
 
     // await toast.promise(updateService({ formData: data }).unwrap(), {
     // 	error: t('could-not-update'),
@@ -91,173 +104,179 @@ const ContactForm = () => {
   };
 
   return (
-    <form
-      className="flex flex-col justify-center items-center gap-5 px-5"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="basis-1/2">
-        <Controller
-          name="name"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => (
-            <Input
-              inputProps={{
-                ...field,
-                id: "name",
-                name: "name",
-                type: "text",
-                placeholder: "name ",
-              }}
-              label={t("name") + "*"}
-            />
-          )}
-        />
-        {errors.name && (
-          <p className="text-xs mb-3 text-red-700">
-            {t("name") + " " + t("requried")}
-          </p>
-        )}
-        <Controller
-          name="email"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => (
-            <Input
-              inputProps={{
-                ...field,
-                id: "email",
-                name: "email",
-                type: "text",
-                placeholder: "email ",
-              }}
-              label={t("email") + "*"}
-            />
-          )}
-        />
-        {errors.email && (
-          <p className="text-xs mb-3 text-red-700">
-            {t("email") + " " + t("requried")}
-          </p>
-        )}
-        <Controller
-          name="mobileNumber"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => (
-            <Input
-              inputProps={{
-                ...field,
-                id: "mobileNumber",
-                name: "mobileNumber",
-                type: "text",
-                placeholder: "Mobile Number ",
-              }}
-              label={t("mobileNumber") + "*"}
-            />
-          )}
-        />
-        {errors.mobileNumber && (
-          <p className="text-xs mb-3 text-red-700">
-            {t("mobileNumber") + " " + t("requried")}
-          </p>
-        )}
-
-        <Controller
-          name="jobTitle"
-          control={control}
-          render={({ field }) => (
-            <Input
-              inputProps={{
-                ...field,
-                id: "jobTitle",
-                name: "jobTitle",
-                type: "text",
-                placeholder: "jobTitle ",
-              }}
-              label={t("jobTitle")}
-            />
-          )}
-        />
-
-        <Controller
-          name="Institution"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => (
-            <Input
-              inputProps={{
-                ...field,
-                id: "Institution",
-                name: "Institution",
-                type: "text",
-                placeholder: "Institution ",
-              }}
-              label={t("institution")}
-            />
-          )}
-        />
-
-        <Controller
-          name="questionRegarding"
-          control={control}
-          render={({ field }) => (
-            <Select
-              className="my-4 border-gray-200 shadow"
-              components={{
-                IndicatorSeparator: () => null,
-              }}
-              styles={selectStyle}
-              theme={selectTheme}
-              placeholder={t("questionRegarding")}
-              {...field}
-              options={questionRegardingOptions}
-            />
-          )}
-        />
-        <Controller
-          name="hearUs"
-          control={control}
-          render={({ field }) => (
-            <Select
-              components={{
-                IndicatorSeparator: () => null,
-              }}
-              styles={selectStyle}
-              theme={selectTheme}
-              className="my-4 border-gray-200 shadow"
-              placeholder={t("hearUs")}
-              {...field}
-              options={hearUsOptions}
-            />
-          )}
-        />
-        <Controller
-          name="time"
-          control={control}
-          render={({ field }) => (
-            <Select
-              components={{
-                IndicatorSeparator: () => null,
-              }}
-              styles={selectStyle}
-              theme={selectTheme}
-              className="my-4 border-gray-200  shadow "
-              placeholder={t("contactTime")}
-              {...field}
-              options={timeOptions}
-            />
-          )}
-        />
-      </div>
-
-      <button
-        type="submit"
-        className="bg-primary w-[300px] sm:w-[400px] text-white py-2 rounded-full"
+    <>
+      <ToastContainer />
+      <form
+        className="flex flex-col justify-center items-center gap-5 px-5"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        {t("Send")}
-      </button>
-    </form>
+        <div className="basis-1/2">
+          <Controller
+            name="name"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Input
+                inputProps={{
+                  ...field,
+                  id: "name",
+                  name: "name",
+                  type: "text",
+                  placeholder: "name ",
+                }}
+                label={t("name") + "*"}
+              />
+            )}
+          />
+          {errors.name && (
+            <p className="text-xs mb-3 text-red-700">
+              {t("name") + " " + t("requried")}
+            </p>
+          )}
+          <Controller
+            name="email"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Input
+                inputProps={{
+                  ...field,
+                  id: "email",
+                  name: "email",
+                  type: "text",
+                  placeholder: "email ",
+                }}
+                label={t("email") + "*"}
+              />
+            )}
+          />
+          {errors.email && (
+            <p className="text-xs mb-3 text-red-700">
+              {t("email") + " " + t("requried")}
+            </p>
+          )}
+          <Controller
+            name="mobileNumber"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Input
+                inputProps={{
+                  ...field,
+                  id: "mobileNumber",
+                  name: "mobileNumber",
+                  type: "text",
+                  placeholder: "Mobile Number ",
+                }}
+                label={t("mobileNumber") + "*"}
+              />
+            )}
+          />
+          {errors.mobileNumber && (
+            <p className="text-xs mb-3 text-red-700">
+              {t("mobileNumber") + " " + t("requried")}
+            </p>
+          )}
+
+          <Controller
+            name="jobTitle"
+            control={control}
+            render={({ field }) => (
+              <Input
+                inputProps={{
+                  ...field,
+                  id: "jobTitle",
+                  name: "jobTitle",
+                  type: "text",
+                  placeholder: "jobTitle ",
+                }}
+                label={t("jobTitle")}
+              />
+            )}
+          />
+
+          <Controller
+            name="Institution"
+            control={control}
+            render={({ field }) => (
+              <Input
+                inputProps={{
+                  ...field,
+                  id: "Institution",
+                  name: "Institution",
+                  type: "text",
+                  placeholder: "Institution ",
+                }}
+                label={t("institution")}
+              />
+            )}
+          />
+
+          <Controller
+            name="questionRegarding"
+            control={control}
+            render={({ field }) => (
+              <Select
+                className="my-4 border-gray-200 shadow"
+                components={{
+                  IndicatorSeparator: () => null,
+                }}
+                styles={selectStyle}
+                theme={selectTheme}
+                placeholder={t("questionRegarding")}
+                {...field}
+                options={questionRegardingOptions}
+              />
+            )}
+          />
+          <Controller
+            name="hearUs"
+            control={control}
+            render={({ field }) => (
+              <Select
+                components={{
+                  IndicatorSeparator: () => null,
+                }}
+                styles={selectStyle}
+                theme={selectTheme}
+                className="my-4 border-gray-200 shadow"
+                placeholder={t("hearUs")}
+                {...field}
+                options={hearUsOptions}
+              />
+            )}
+          />
+          <Controller
+            name="time"
+            control={control}
+            render={({ field }) => (
+              <Select
+                components={{
+                  IndicatorSeparator: () => null,
+                }}
+                styles={selectStyle}
+                theme={selectTheme}
+                className="my-4 border-gray-200  shadow "
+                placeholder={t("contactTime")}
+                {...field}
+                options={timeOptions}
+              />
+            )}
+          />
+        </div>
+        <ReCAPTCHA
+          sitekey="6LeK1O0pAAAAALRCLnjlY-hq0c5rpp5og9t-DgFK"
+          ref={recaptcha}
+        />
+
+        <button
+          type="submit"
+          className="bg-primary w-[300px] sm:w-[400px] text-white py-2 rounded-full"
+        >
+          {t("Send")}
+        </button>
+      </form>
+    </>
   );
 };
 
