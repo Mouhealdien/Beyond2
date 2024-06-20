f49162ae970e143078c28637f7c10d9d61ece3f1
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-06-20 15:27:51 +0300

    adding render backend link

--- constants/settings.ts
+++ constants/settings.ts
@@ -1,4 +1,4 @@
-//export const SERVER_URL = 'https://efac-5-193-21-208.ngrok-free.app/';
-export const SERVER_URL = 'http://127.0.0.1:8080/';
+export const SERVER_URL = 'https://beyondbackend-3.onrender.com/';
+//export const SERVER_URL = 'http://127.0.0.1:8080/';
 //'http://127.0.0.1:8080'
 export const BASE_API_URL = SERVER_URL ;
