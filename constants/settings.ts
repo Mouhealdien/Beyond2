59b1596c4e7ce728e766e92d9c9daf3df14a1fda
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-07-04 00:22:30 +0300

    adding backend link

--- constants/settings.ts
+++ constants/settings.ts
@@ -1,4 +1,5 @@
-export const SERVER_URL = 'https://beyondbackend-3.onrender.com/';
+//export const SERVER_URL = 'https://beyondbackend-3.onrender.com/';
 //export const SERVER_URL = 'http://127.0.0.1:8080/';
 //'http://127.0.0.1:8080'
+export const SERVER_URL = "https://server.beyond4m.com/";
 export const BASE_API_URL = SERVER_URL ;
