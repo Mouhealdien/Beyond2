acfb88f90d6d08dd5650ef95164ad61a212409b7
Mouhealdien <mhyaldynalamyn45@gmail.com>
2024-10-05 18:23:47 +0300

    fix project card styles

--- components/our works/Projects.tsx
+++ components/our works/Projects.tsx
@@ -1,14 +1,12 @@
 import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import work from "../../public/assets/works.png";
 import { FaArrowRight } from "react-icons/fa";
 import useMediaQuery from "../../lib/hooks/hooks/useMediaQuery";
 import { useTranslation } from "next-i18next";
 import { Project } from "../../common/types/projects";
 import { getPhotoUrl } from "../../lib/utils";
 const Projects = ({ projects }) => {
-  const arr = [1, 2, 3];
   const istab = useMediaQuery("(min-width: 1025px)");
   const { i18n, t } = useTranslation();
   const language = i18n.language === "en" ? "en" : "ar";
@@ -22,14 +20,14 @@ const Projects = ({ projects }) => {
           <div
             key={i}
             style={{ marginTop: `${mt}` }}
-            className={`pb-5 sm:pb-1  max-w-[200px] sm:max-w-[300px] md:max-w-[260px] xl:max-w-[300px] h-[300px] sm:h-[450px]`}
+            className={`pb-5 sm:pb-1  max-w-[200px] sm:max-w-[300px] md:max-w-[260px] xl:max-w-[300px] `}
           >
             <Link
               className="text-primary px-10 text-[10px] md:text-base"
               href={`our-works/${e._id}`}
             >
               <a>
-                <div className=" rounded-xl h-[70%] xxs:h-[80%] mb-2  overflow-hidden  ">
+                <div className=" rounded-xl mb-1  overflow-hidden  ">
                   <Image
                     style={{ transform: "scale(2.5)" }}
                     className="rounded-t-lg   "
@@ -61,21 +59,3 @@ const Projects = ({ projects }) => {
 };
 
 export default Projects;
-
-{
-  /* <div key={i} style={{marginTop:`${mt}`}} className={`flex flex-col`}>
-                  <div className="overflow-hidden rounded-md max-w-[9rem] md:max-w-[16rem] lg:max-w-[350px] h-[15rem]  lg:h-[25rem] ">
-                    <Image
-                      style={{ transform: "scale(2.5)" }}
-                      className="   "
-                      src={work}
-                      alt="work"
-                    />
-                  </div>
-                  <h1 className="text-[20px]  sm:text-green-700 md:text-blue-700 lg:text-red-500 font-bold">hello world</h1>
-                  <Link className="text-primary px-5" href={"#"}>
-                    show full project{" "}
-                    <FaArrowRight width={3} className="inline-block" />{" "}
-                  </Link>
-                </div>  */
-}
